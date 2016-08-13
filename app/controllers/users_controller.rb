cfb1bfb18745746c8c0305b6bdf6398b9cf7956c
Chris Roma <chrisroma23@gmail.com>
2016-08-13 15:14:54 -0400

    Change the name to Roma's Twitter Clone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)    
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to TwitterClone!"
+      flash[:success] = "Welcome to Roma's Twitter Clone!"
       redirect_to @user
     else
       render 'new'
